a5d6705622714e452caf779ff6019ff06f7f8ac1
Hussein Kadweka <developersjuniors@gmail.com>
2019-10-16 15:26:11 +0300

    add an image and styled it to circle shape and add content to the typescript part

--- javascript/enyewe.js
+++ javascript/enyewe.js
@@ -1,7 +1,8 @@
 
   var typed = new Typed('.moringa', {
     strings: ["Hussein Kadweka",
-    "Software Developer"],
+    "Software Developer",
+        'In Web Applications'],
     typeSpeed:60,
     backSpeed:60,
     loop:true
